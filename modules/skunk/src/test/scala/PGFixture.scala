1c1b84df9657d63242653676756823e00888f1f0
Hossein Naderi <hossein-naderi@hotmail.com>
2022-02-12 07:34:14 +0330

    fixed wrong db fixture connection

--- modules/skunk/src/test/scala/PGFixture.scala
+++ modules/skunk/src/test/scala/PGFixture.scala
@@ -9,9 +9,11 @@ import cats.implicits.*
 
 object PGFixture {
   val pgHost = sys.env.getOrElse("POSTGRES_HOST", "localhost")
-  val pgPort = sys.env.get("POSTGRES_PORT").map(_.toInt).getOrElse(5432)
-  val pgUser = sys.env.getOrElse("POSTGRES_USERNAME", "postgres")
+  val pgPort =
+    sys.env.get("POSTGRES_PORT").flatMap(_.toIntOption).getOrElse(5432)
+  val pgUser = sys.env.getOrElse("POSTGRES_USER", "postgres")
   val pgPass = sys.env.get("POSTGRES_PASSWORD").orElse(Some("postgres"))
+  val pgDb = sys.env.getOrElse("POSTGRES_DB", "postgres")
 
   val pool: Resource[IO, Resource[IO, Session[IO]]] = pooled(10)
 
@@ -20,7 +22,7 @@ object PGFixture {
       host = pgHost,
       port = pgPort,
       user = pgUser,
-      database = "postgres",
+      database = pgDb,
       password = pgPass,
       max = size
     )
