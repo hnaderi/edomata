6ef672f844f0ba405de821e6b6f433a8f24e5a9d
Hossein Naderi <hossein-naderi@hotmail.com>
2022-03-30 21:49:25 +0430

    Simplified DomainDSL2

--- modules/core/src/main/scala/DomainDSL2.scala
+++ modules/core/src/main/scala/DomainDSL2.scala
@@ -10,7 +10,7 @@ import java.time.Instant
 
 import Domain.*
 
-final class DomainDSL2[C, S, E, R, N, M] {
+final class DomainDSL2[F[_]: Monad, C, S, E, R, N, M] {
   type DomainModel = S & Model[S, E, R]
   type Decision[T] = edomata.core.Decision[R, E, T]
   type Logic[F[_], T] = DecisionT[F, R, E, T]
@@ -21,43 +21,44 @@ final class DomainDSL2[C, S, E, R, N, M] {
   type Service[F[_], T] = DecisionT[RequestOf[F], R, E, T]
   type ServiceOf[F[_]] = [t] =>> Service[F, t]
 
-  def router[F[_]: Monad](
+  def router(
       f: C => Service[F, Unit]
   ): Service[F, Unit] =
     command.flatMap(f)
 
-  def eval[F[_]: Monad, T](ft: F[T]): Service[F, T] =
+  def eval[T](ft: F[T]): Service[F, T] =
     DecisionT.liftF(RequestMonad.liftF(ft))
 
-  def ask[F[_]: Monad]: Service[F, Context] = DecisionT.liftF(RequestMonad.ask)
-  def command[F[_]: Monad]: Service[F, C] = ask.map(_.command)
-  def metadata[F[_]: Monad]: Service[F, M] = ask.map(_.metadata)
-  def aggregateId[F[_]: Monad]: Service[F, String] = ask.map(_.aggregateId)
-  def messageId[F[_]: Monad]: Service[F, String] = ask.map(_.id)
-  def state[F[_]: Monad]: Service[F, S] = ask.map(_.state)
+  def ask: Service[F, Context] =
+    DecisionT.liftF(RequestMonad.ask)
+  def command: Service[F, C] = ask.map(_.command)
+  def metadata: Service[F, M] = ask.map(_.metadata)
+  def aggregateId: Service[F, String] = ask.map(_.aggregateId)
+  def messageId: Service[F, String] = ask.map(_.id)
+  def state: Service[F, S] = ask.map(_.state)
 
-  def accept[F[_]: Monad](e: E, es: E*): Service[F, Unit] =
+  def accept(e: E, es: E*): Service[F, Unit] =
     DecisionT.accept(e, es: _*)
 
-  def acceptReturn[F[_]: Monad, T](t: T)(e: E, es: E*): Service[F, T] =
+  def acceptReturn[T](t: T)(e: E, es: E*): Service[F, T] =
     DecisionT.acceptReturn(t)(e, es: _*)
 
-  def reject[F[_]: Monad, T](reason: R, more: R*): Service[F, T] =
+  def reject[T](reason: R, more: R*): Service[F, T] =
     DecisionT.lift(Decision.reject(reason, more: _*))
 
-  def validate[F[_]: Monad, T](v: ValidatedNec[R, T]): Service[F, T] =
+  def validate[T](v: ValidatedNec[R, T]): Service[F, T] =
     DecisionT.validate(v)
 
-  def pure[F[_]: Monad, T](t: T): Service[F, T] =
+  def pure[T](t: T): Service[F, T] =
     DecisionT.pure(t)
 
-  def publish[F[_]: Monad](n: N*): Service[F, Unit] =
+  def publish(n: N*): Service[F, Unit] =
     DecisionT.liftF(RequestMonad.publish(n: _*))
 
-  def publishReturn[F[_]: Monad, T](t: T)(n: N*): Service[F, T] =
+  def publishReturn[T](t: T)(n: N*): Service[F, T] =
     DecisionT.liftF(RequestMonad.publishReturn(t)(n: _*))
 
-  def handle[F[_]: Monad, T](
+  def handle[T](
       decision: Decision[T]
   ): Service[F, T] =
     DecisionT.lift(decision)
@@ -65,7 +66,7 @@ final class DomainDSL2[C, S, E, R, N, M] {
 
 object DomainDSL2 {
   import DomainType.*
-  def build[D]: DomainDSL2[CommandFor[D], StateFor[D], EventFor[
+  def build[F[_]: Monad, D]: DomainDSL2[F, CommandFor[D], StateFor[D], EventFor[
     D
   ], RejectionFor[D], NotificationFor[D], MetadataFor[D]] = new DomainDSL2
 }
