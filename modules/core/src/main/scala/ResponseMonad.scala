58e5c2468ce45f2b4274917154454d8f7b96eae6
Hossein Naderi <hossein-naderi@hotmail.com>
2022-03-31 12:23:59 +0430

    Added ResponseMonad
    This is a data structure that implements a writer monad for
    notifications, which is resets when rejected.

---
+++ modules/core/src/main/scala/ResponseMonad.scala
@@ -0,0 +1,146 @@
+package edomata.core
+
+import cats.Applicative
+import cats.Functor
+import cats.Monad
+import cats.MonadError
+import cats.data.Chain
+import cats.data.NonEmptyChain
+import cats.data.ValidatedNec
+import cats.implicits.*
+
+import scala.annotation.tailrec
+
+final case class ResponseMonad[R, E, N, +A](
+    result: Decision[R, E, A],
+    notifications: Seq[N] = Nil
+) {
+  def map[B](f: A => B): ResponseMonad[R, E, N, B] =
+    copy(result = result.map(f))
+
+  def flatMap[N2 >: N, B](
+      f: A => ResponseMonad[R, E, N2, B]
+  ): ResponseMonad[R, E, N2, B] = result.fold(
+    rej => ResponseMonad(Decision.Rejected(rej), notifications),
+    a => {
+      val out = f(a)
+      (result >> out.result) match {
+        case d @ Decision.Rejected(_) => ResponseMonad(d)
+        case other => ResponseMonad(other, notifications ++ out.notifications)
+      }
+    }
+  )
+
+  def as[B](b: B): ResponseMonad[R, E, N, B] = map(_ => b)
+
+  /** Clears all notifications so far */
+  def reset: ResponseMonad[R, E, N, A] =
+    copy(notifications = Nil)
+
+  /** Adds notification without considering decision state */
+  def publish(ns: N*): ResponseMonad[R, E, N, A] =
+    copy(notifications = notifications ++ ns)
+
+  def publishOnRejectionWith(
+      f: NonEmptyChain[R] => Seq[N]
+  ): ResponseMonad[R, E, N, A] = publish(result.fold(f, _ => Nil): _*)
+  def publishOnRejection(ns: N*): ResponseMonad[R, E, N, A] =
+    publishOnRejectionWith(_ => ns)
+
+}
+
+object ResponseMonad extends ResponseMonadConstructors {
+  given [R, E, N]
+      : MonadError[[t] =>> ResponseMonad[R, E, N, t], NonEmptyChain[R]] =
+    new MonadError {
+      override def map[A, B](fa: ResponseMonad[R, E, N, A])(
+          f: A => B
+      ): ResponseMonad[R, E, N, B] = fa.map(f)
+
+      def flatMap[A, B](fa: ResponseMonad[R, E, N, A])(
+          f: A => ResponseMonad[R, E, N, B]
+      ): ResponseMonad[R, E, N, B] = fa.flatMap(f)
+
+      @tailrec
+      private def step[A, B](
+          a: A,
+          evs: Chain[E] = Chain.empty,
+          ns: Seq[N] = Nil
+      )(
+          f: A => ResponseMonad[R, E, N, Either[A, B]]
+      ): ResponseMonad[R, E, N, B] =
+        val out = f(a)
+        out.result match {
+          case Decision.Accepted(ev2, Left(a)) =>
+            step(a, evs ++ ev2.toChain, ns ++ out.notifications)(f)
+          case Decision.Accepted(ev2, Right(b)) =>
+            ResponseMonad(
+              Decision.Accepted(ev2.prependChain(evs), b),
+              ns ++ out.notifications
+            )
+          case Decision.InDecisive(Left(a)) =>
+            step(a, evs, ns ++ out.notifications)(f)
+          case Decision.InDecisive(Right(b)) =>
+            ResponseMonad(
+              NonEmptyChain
+                .fromChain(evs)
+                .fold(Decision.InDecisive(b))(Decision.Accepted(_, b)),
+              ns ++ out.notifications
+            )
+          case Decision.Rejected(rs) => ResponseMonad(Decision.Rejected(rs))
+        }
+
+      def tailRecM[A, B](
+          a: A
+      )(
+          f: A => ResponseMonad[R, E, N, Either[A, B]]
+      ): ResponseMonad[R, E, N, B] =
+        step(a)(f)
+
+      def pure[A](x: A): ResponseMonad[R, E, N, A] =
+        ResponseMonad(Decision.pure(x))
+
+      def handleErrorWith[A](
+          fa: ResponseMonad[R, E, N, A]
+      )(
+          f: NonEmptyChain[R] => ResponseMonad[R, E, N, A]
+      ): ResponseMonad[R, E, N, A] =
+        fa.result match {
+          case Decision.Rejected(e) => f(e)
+          case other                => fa
+        }
+
+      def raiseError[A](e: NonEmptyChain[R]): ResponseMonad[R, E, N, A] =
+        ResponseMonad(Decision.Rejected(e))
+    }
+}
+
+sealed trait ResponseMonadConstructors {
+  def pure[R, E, N, T](t: T): ResponseMonad[R, E, N, T] = ResponseMonad(
+    Decision.pure(t)
+  )
+
+  def unit[R, E, N]: ResponseMonad[R, E, N, Unit] = pure(())
+
+  def publish[R, E, N](n: N*): ResponseMonad[R, E, N, Unit] =
+    ResponseMonad(Decision.unit, n)
+
+  def accept[R, E, N](ev: E, evs: E*): ResponseMonad[R, E, N, Unit] =
+    acceptReturn(())(ev, evs: _*)
+
+  def acceptReturn[R, E, N, T](
+      t: T
+  )(ev: E, evs: E*): ResponseMonad[R, E, N, T] =
+    ResponseMonad(Decision.Accepted(NonEmptyChain.of(ev, evs: _*), t))
+
+  def reject[R, E, N](
+      reason: R,
+      otherReasons: R*
+  ): ResponseMonad[R, E, N, Nothing] =
+    ResponseMonad(Decision.Rejected(NonEmptyChain.of(reason, otherReasons: _*)))
+
+  def validate[R, E, N, T](
+      validation: ValidatedNec[R, T]
+  ): ResponseMonad[R, E, N, T] =
+    ResponseMonad(Decision.validate(validation))
+}
---
+++ modules/core/src/test/scala/ResponseMonadTest.scala
@@ -0,0 +1,100 @@
+package edomata.core
+
+import cats.Eval
+import cats.Monad
+import cats.data.Kleisli
+import cats.data.NonEmptyChain
+import cats.implicits.*
+import munit.*
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+
+import ResponseMonadTest.*
+import DecisionTest.*
+
+class ResponseMonadTest extends FunSuite, ScalaCheckSuite {
+  property("Accumulates on accept") {
+    forAll(notRejected, notifications, notRejected, notifications) {
+      (a1, n1, a2, n2) =>
+        val r1 = ResponseMonad(a1, n1)
+        val r2 = ResponseMonad(a2, n2)
+        val r3 = r1 >> r2
+        assertEquals(r3, ResponseMonad(a1.flatMap(_ => a2), n1 ++ n2))
+    }
+  }
+  property("Resets on rejection") {
+    forAll(notRejected, notifications, rejected, notifications) {
+      (a1, n1, a2, n2) =>
+        val r1 = ResponseMonad(a1, n1)
+        val r2 = ResponseMonad(a2, n2)
+        val r3 = r1 >> r2
+        assertEquals(r3, ResponseMonad(a1.flatMap(_ => a2), Nil))
+        assert(r3.result.isRejected)
+    }
+  }
+  property("Rejected does not change") {
+    forAll(rejected, notifications, anySut, notifications) { (a1, n1, a2, n2) =>
+      val r1 = ResponseMonad(a1, n1)
+      val r2 = ResponseMonad(a2, n2)
+      val r3 = r1 >> r2
+      assertEquals(r3, r1)
+      assert(r3.result.isRejected)
+    }
+  }
+  property("Publish on rejection") {
+    forAll(rejected, notifications, notifications) { (a1, n1, n2) =>
+      val r1 = ResponseMonad(a1, n1)
+      val r2 = r1.publishOnRejection(n2: _*)
+
+      assertEquals(
+        r2,
+        r1.copy(notifications = r1.notifications ++ n2)
+      )
+    }
+  }
+  property("Error handling") {
+    forAll(rejected, notifications, notifications) { (a1, n1, n2) =>
+      val r1 = ResponseMonad(a1, n1)
+      val r2 = r1.handleErrorWith { case r =>
+        ResponseMonad(Decision.Rejected(r), n2)
+      }
+      assertEquals(
+        r2,
+        r1.copy(notifications = n2)
+      )
+    }
+  }
+  property("Publish adds notifications") {
+    forAll(anySut, notifications, notifications) { (a1, n1, n2) =>
+      val r1 = ResponseMonad(a1, n1)
+      val r2 = r1.publish(n2: _*)
+      assertEquals(r2, r1.copy(notifications = n1 ++ n2))
+    }
+  }
+  property("Reset clears notifications") {
+    forAll(anySut, notifications) { (a1, n1) =>
+      val r1 = ResponseMonad(a1, n1)
+      val r2 = r1.reset
+      assertEquals(r2.notifications, Nil)
+      assertEquals(r2.result, r1.result)
+    }
+  }
+
+  test("tail rec") {
+    val n = 10
+    val c = Monad[Res].tailRecM(0) { a =>
+      if (a < n) then ResponseMonad.acceptReturn(Left(a + 1))(a)
+      else ResponseMonad.pure(a.asRight)
+    }
+    assertEquals(c, ResponseMonad.acceptReturn(n)(0, (1 to n - 1): _*))
+  }
+}
+
+object ResponseMonadTest {
+  final case class Notification(value: String = "")
+
+  type Res[T] = ResponseMonad[Rejection, Event, Notification, T]
+  private val notifications =
+    Gen.containerOf[Seq, Notification](arbitrary[String].map(Notification(_)))
+}
