edf76bb57fab5992782fcffb0a6a02642286fe05
Hossein Naderi <hossein-naderi@hotmail.com>
2022-04-15 15:41:57 +0430

    Implemented DoobieBackend

--- modules/doobie/src/main/scala/DoobieBackend.scala
+++ modules/doobie/src/main/scala/DoobieBackend.scala
@@ -1,24 +1,85 @@
 package edomata.backend
 
+import cats.data.EitherNec
 import cats.data.NonEmptyChain
 import cats.effect.Concurrent
+import cats.effect.kernel.Clock
 import cats.effect.kernel.Resource
+import cats.implicits.*
 import doobie.ConnectionIO
+import doobie.FC
 import doobie.Transactor
 import doobie.implicits.*
+import edomata.core.CommandMessage
+import edomata.core.Compiler
 import edomata.core.Domain
 import edomata.core.ModelTC
+import edomata.core.ProgramResult
+import edomata.core.RequestContext
 import fs2.Stream
 
-final class DoobieBackend[F[_]: Concurrent, S, E, R, N] private (
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+private val void: EitherNec[Nothing, Unit] = Right(())
+
+final class DoobieBackend[F[_], S, E, R, N] private (
     _journal: Journal[ConnectionIO, E],
     _outbox: Outbox[ConnectionIO, N],
     snapshot: SnapshotStore[F, S, E, R],
     command: CommandStore[ConnectionIO],
     trx: Transactor[F]
-)(using m: edomata.core.ModelTC[S, E, R])
+)(using m: ModelTC[S, E, R], F: Concurrent[F], clock: Clock[F])
     extends Backend[F, S, E, R, N] {
-  def compiler[C]: edomata.core.Compiler[F, C, S, E, R, N] = ???
+
+  private def publish(
+      notifs: Seq[N],
+      cmd: CommandMessage[?],
+      time: OffsetDateTime
+  ): F[EitherNec[R, Unit]] =
+    NonEmptyChain
+      .fromSeq(notifs)
+      .fold(void.pure)(
+        _outbox.write(_, time, cmd.metadata).transact(trx).as(void)
+      )
+
+  private def publishCIO(
+      notifs: Seq[N],
+      cmd: CommandMessage[?],
+      time: OffsetDateTime
+  ): ConnectionIO[Unit] =
+    NonEmptyChain
+      .fromSeq(notifs)
+      .fold(FC.unit)(
+        _outbox.write(_, time, cmd.metadata)
+      )
+
+  private def currentTime =
+    clock.realTimeInstant.map(_.atOffset(ZoneOffset.UTC))
+
+  def compiler[C]: Compiler[F, C, S, E, R, N] = new {
+    def onRequest(cmd: CommandMessage[C])(
+        run: RequestContext[C, S] => F[ProgramResult[S, E, R, N]]
+    ): F[EitherNec[R, Unit]] =
+      currentTime.flatMap { now =>
+        repository.get(cmd.id).flatMap {
+          case AggregateState.Valid(s, version) =>
+            run(cmd.buildContext(s)).flatMap {
+              case ProgramResult.Accepted(ns, evs, notifs) =>
+                (_journal.append(streamId = cmd.address, now, version, evs) >>
+                  publishCIO(notifs, cmd, now) >>
+                  command.append(cmd)).transact(trx).as(void)
+              case ProgramResult.Indecisive(notifs) =>
+                publish(notifs, cmd, now)
+              case ProgramResult.Rejected(notifs, errs) =>
+                publish(notifs, cmd, now).as(errs.asLeft)
+              case ProgramResult.Conflicted(errs) => errs.asLeft.pure
+            }
+          case AggregateState.Conflicted(ls, ev, errs) =>
+            errs.asLeft.pure
+        }
+      }
+  }
 
   val outbox: OutboxReader[F, N] = DoobieOutboxReader(trx, _outbox)
   val journal: JournalReader[F, E] = DoobieJournalReader(trx, _journal)
