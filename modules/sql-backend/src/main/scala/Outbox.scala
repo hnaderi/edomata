a94b3bff37c768edda9a65e810d7dfdcb841bb02
Hossein Naderi <hossein-naderi@hotmail.com>
2022-04-15 15:41:24 +0430

    Changed OutboxWriter to accept NonEmptyChain

--- modules/sql-backend/src/main/scala/Outbox.scala
+++ modules/sql-backend/src/main/scala/Outbox.scala
@@ -7,21 +7,25 @@ import fs2.Stream
 import java.time.OffsetDateTime
 import java.util.UUID
 
-trait Outbox[F[_], T] extends OutboxReader[F, T], OutboxWriter[F, T]
+trait Outbox[F[_], N] extends OutboxReader[F, N], OutboxWriter[F, N]
 
-trait OutboxWriter[F[_], T] {
-  def write(t: T, time: OffsetDateTime, meta: MessageMetadata): F[Unit]
+trait OutboxWriter[F[_], N] {
+  def write(
+      t: NonEmptyChain[N],
+      time: OffsetDateTime,
+      meta: MessageMetadata
+  ): F[Unit]
 }
-trait OutboxReader[F[_], T] {
-  def read: Stream[F, OutboxItem[T]]
-  def markAllAsSent(items: NonEmptyChain[OutboxItem[T]]): F[Unit]
-  def markAsSent(item: OutboxItem[T], others: OutboxItem[T]*): F[Unit] =
+trait OutboxReader[F[_], N] {
+  def read: Stream[F, OutboxItem[N]]
+  def markAllAsSent(items: NonEmptyChain[OutboxItem[N]]): F[Unit]
+  def markAsSent(item: OutboxItem[N], others: OutboxItem[N]*): F[Unit] =
     markAllAsSent(NonEmptyChain.of(item, others: _*))
 }
 
-final case class OutboxItem[T](
+final case class OutboxItem[N](
     seqNr: SeqNr,
     time: OffsetDateTime,
-    data: T,
+    data: N,
     metadata: MessageMetadata
 )
