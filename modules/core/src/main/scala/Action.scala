8817bc4d2cd9b4af5c878cccfb0451dbaa9a12e4
Hossein Naderi <hossein-naderi@hotmail.com>
2022-04-20 21:06:04 +0430

    Added law tests for Action

--- modules/core/src/main/scala/Action.scala
+++ modules/core/src/main/scala/Action.scala
@@ -3,66 +3,38 @@ package edomata.core
 import cats.Applicative
 import cats.Functor
 import cats.Monad
+import cats.data.Chain
+import cats.data.NonEmptyChain
 import cats.data.ValidatedNec
 import cats.implicits._
-import edomata.core.Decision.Accepted
-import edomata.core.Decision.InDecisive
-import edomata.core.Decision.Rejected
+import cats.kernel.Eq
 
-final case class ActionResult[R, E, N, T](
-    decision: Decision[R, E, T],
-    notifications: Seq[N] = Nil
-)
-
-final case class Action[F[_], R, E, N, A](run: F[ActionResult[R, E, N, A]]) {
+final case class Action[F[_], R, E, N, A](run: F[Response[R, E, N, A]]) {
   def map[B](f: A => B)(using F: Functor[F]): Action[F, R, E, N, B] =
-    Action(F.map(run)(d => d.copy(decision = d.decision.map(f))))
+    Action(F.map(run)(_.map(f)))
 
   def flatMap[R2 >: R, E2 >: E, N2 >: N, B](
       f: A => Action[F, R2, E2, N2, B]
   )(using M: Monad[F]): Action[F, R2, E2, N2, B] =
     Action {
-      M.flatMap(run) { case ActionResult(decision, notifs) =>
-        decision match {
-          case e1 @ Accepted(events1, result) =>
-            f(result).run.map { res =>
-              res.decision match {
-                case e2 @ Accepted(events2, _) =>
-                  ActionResult(
-                    e2.copy(events = events1 ++ events2),
-                    notifs ++ res.notifications
-                  )
-                case InDecisive(result) =>
-                  ActionResult(
-                    e1.copy(result = result),
-                    notifs ++ res.notifications
-                  )
-                case r @ Rejected(_) => ActionResult(r.copy(), Nil)
-              }
-            }
-          case InDecisive(result) =>
-            f(result).run.map(res =>
-              res.decision match {
-                case r @ Rejected(_) => res
-                case r =>
-                  res.copy(notifications = notifs ++ res.notifications)
-              }
-            )
-          case r @ Rejected(_) => M.pure(ActionResult(r.copy(), notifs))
-        }
-      }
+      M.flatMap(run)(a =>
+        a.result.visit(
+          err => a.copy(result = Decision.Rejected(err)).pure,
+          aa => f(aa).run.map(a >> _)
+        )
+      )
     }
 
   def as[B](b: B)(using F: Functor[F]): Action[F, R, E, N, B] = map(_ => b)
 
   /** Clears all notifications so far */
   def reset(using F: Functor[F]): Action[F, R, E, N, A] =
-    Action(run.map(_.copy(notifications = Nil)))
+    Action(run.map(_.reset))
 
   /** Adds notification without considering decision state */
   def publish(ns: N*)(using F: Functor[F]): Action[F, R, E, N, A] =
     Action(
-      run.map(res => res.copy(notifications = res.notifications ++ ns))
+      run.map(_.publish(ns: _*))
     )
 }
 
@@ -70,26 +42,26 @@ object Action extends ActionConstructors with ActionCatsInstances
 
 sealed transparent trait ActionConstructors {
   def lift[F[_], R, E, N, T](
-      t: ActionResult[R, E, N, T]
+      t: Response[R, E, N, T]
   )(using F: Applicative[F]): Action[F, R, E, N, T] =
     Action(F.pure(t))
 
   def liftD[F[_], R, E, N, T](
       t: Decision[R, E, T]
   )(using F: Applicative[F]): Action[F, R, E, N, T] =
-    lift(ActionResult(t))
+    lift(Response(t))
 
   def pure[F[_], R, E, N, T](
       t: T
   )(using F: Applicative[F]): Action[F, R, E, N, T] =
-    lift(ActionResult(Decision.pure(t)))
+    lift(Response(Decision.pure(t)))
 
   def void[F[_]: Applicative, R, E, N]: Action[F, R, E, N, Unit] = pure(())
 
   def liftF[F[_], R, E, N, T](
       f: F[T]
   )(using F: Functor[F]): Action[F, R, E, N, T] =
-    Action(F.map(f)(d => ActionResult(Decision.pure(d))))
+    Action(F.map(f)(d => Response(Decision.pure(d))))
 
   def validate[F[_]: Applicative, R, E, N, T](
       validation: ValidatedNec[R, T]
@@ -111,7 +83,7 @@ sealed transparent trait ActionConstructors {
   def publish[F[_]: Applicative, R, E, N](
       ns: N*
   ): Action[F, R, E, N, Unit] =
-    lift(ActionResult(Decision.unit, ns))
+    lift(Response(Decision.unit, Chain.fromSeq(ns)))
 }
 
 sealed transparent trait ActionCatsInstances {
@@ -135,16 +107,42 @@ sealed transparent trait ActionCatsInstances {
         a: A
     )(f: A => Action[F, R, E, N, Either[A, B]]): Action[F, R, E, N, B] =
       Action {
-        F.tailRecM(a) { a =>
+        F.tailRecM((a, Chain.empty[N], Chain.empty[E])) { (a, ns0, evs0) =>
           f(a).run.map { res =>
-            val newDecision = res.decision match {
-              case e @ Accepted(events @ _, result) =>
-                result.map(b => e.copy(result = b))
-              case e @ InDecisive(result) => result.map(b => e.copy(result = b))
-              case e @ Rejected(reasons)  => Right(Rejected(reasons))
+            res.result match {
+              case Decision.Accepted(evs, eab) =>
+                eab.fold(
+                  a =>
+                    (a, ns0 ++ res.notifications, evs0 ++ evs.toChain).asLeft,
+                  b =>
+                    Response(
+                      Decision.Accepted(evs.prependChain(evs0), b),
+                      ns0 ++ res.notifications
+                    ).asRight
+                )
+              case Decision.InDecisive(eab) =>
+                eab.fold(
+                  a => (a, ns0 ++ res.notifications, evs0).asLeft,
+                  b =>
+                    NonEmptyChain
+                      .fromChain(evs0)
+                      .fold(
+                        Response(
+                          Decision.InDecisive(b),
+                          ns0 ++ res.notifications
+                        )
+                      )(evs =>
+                        Response(
+                          Decision.Accepted(evs, b),
+                          ns0 ++ res.notifications
+                        )
+                      )
+                      .asRight
+                )
+              case Decision.Rejected(err) =>
+                res.copy(result = Decision.Rejected(err)).pure
             }
 
-            newDecision.map(ActionResult(_, res.notifications))
           }
         }
       }
@@ -152,4 +150,7 @@ sealed transparent trait ActionCatsInstances {
     def pure[A](x: A): Action[F, R, E, N, A] =
       Action.pure(x)
 
+  given [F[_], R, E, N, A](using
+      Eq[F[Response[R, E, N, A]]]
+  ): Eq[Action[F, R, E, N, A]] = Eq.by(_.run)
 }
--- modules/core/src/test/scala/ActionSuite.scala
+++ modules/core/src/test/scala/ActionSuite.scala
@@ -1,23 +1,26 @@
 package edomata.core
 
 import cats.Eval
+import cats.data.Chain
 import cats.data.Kleisli
 import cats.data.NonEmptyChain
 import cats.implicits.*
+import cats.kernel.laws.discipline.EqTests
+import cats.laws.discipline.MonadTests
 import munit.*
-
-import ActionTest.*
+import org.scalacheck.Arbitrary
 import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Arbitrary
 
-class ActionTest extends FunSuite, ScalaCheckSuite {
+import ActionSuite.*
+
+class ActionSuite extends DisciplineSuite {
   test("Empty action") {
     val a: SUT[Unit] = Action.void
 
     val res = a.run.value
 
-    assertEquals(res.decision, Decision.InDecisive(()))
+    assertEquals(res.result, Decision.InDecisive(()))
     assert(res.notifications.isEmpty, "Non empty notifications!")
   }
 
@@ -30,23 +33,23 @@ class ActionTest extends FunSuite, ScalaCheckSuite {
 
     val res = sut.run.value
 
-    assertEquals(res.decision, Decision.InDecisive(()))
-    assertEquals(res.notifications, Seq(10L))
+    assertEquals(res.result, Decision.InDecisive(()))
+    assertEquals(res.notifications, Chain(10L))
 
     val res2 = sut.publish(20).run.value
 
-    assertEquals(res2.decision, Decision.InDecisive(()))
-    assertEquals(res2.notifications, Seq(10L, 20L))
+    assertEquals(res2.result, Decision.InDecisive(()))
+    assertEquals(res2.notifications, Chain(10L, 20L))
 
     val res3 = sut.reset.run.value
 
-    assertEquals(res3.decision, Decision.InDecisive(()))
+    assertEquals(res3.result, Decision.InDecisive(()))
     assert(res3.notifications.isEmpty, "Non empty notifications!")
 
     val res4 = sutError.run.value
 
-    assertEquals(res4.decision, Decision.Rejected(NonEmptyChain("Some error")))
-    assertEquals(res4.notifications, Seq(20L))
+    assertEquals(res4.result, Decision.Rejected(NonEmptyChain("Some error")))
+    assertEquals(res4.notifications, Chain(20L))
   }
 
   test("Decision") {
@@ -67,24 +70,24 @@ class ActionTest extends FunSuite, ScalaCheckSuite {
 
     val res = sut.run.value
 
-    assertEquals(res.decision, Decision.InDecisive(100))
-    assertEquals(res.notifications, Seq(10L))
+    assertEquals(res.result, Decision.InDecisive(100))
+    assertEquals(res.notifications, Chain(10L))
 
     val errorSut2: SUT[Int] = acceptSut >> Action.reject("Some error")
     val res2 = errorSut.run.value
     val res3 = errorSut2.run.value
 
     assertEquals(res2, res3)
-    assertEquals(res2.decision, Decision.Rejected(NonEmptyChain("Some error")))
-    assertEquals(res2.notifications, Nil)
+    assertEquals(res2.result, Decision.Rejected(NonEmptyChain("Some error")))
+    assertEquals(res2.notifications, Chain.nil)
 
     val res4 = acceptSut.run.value
 
     assertEquals(
-      res4.decision,
+      res4.result,
       Decision.Accepted(NonEmptyChain(101, 102, 103), 200)
     )
-    assertEquals(res4.notifications, Seq(10L))
+    assertEquals(res4.notifications, Chain(10L))
   }
 
   property("Accumulates notifications on accepted") {
@@ -96,8 +99,8 @@ class ActionTest extends FunSuite, ScalaCheckSuite {
       val cres = c.run.value
 
       assertEquals(
-        cres.decision,
-        ares.decision >> bres.decision
+        cres.result,
+        ares.result >> bres.result
       ) // just for doc
       assertEquals(cres.notifications, ares.notifications ++ bres.notifications)
     }
@@ -110,18 +113,18 @@ class ActionTest extends FunSuite, ScalaCheckSuite {
       val bres = b.run.value
       val cres = c.run.value
 
-      assertEquals(cres.decision, ares.decision) // just for doc
+      assertEquals(cres.result, ares.result) // just for doc
       assertEquals(cres.notifications, ares.notifications)
     }
   }
   property("adds notification to rejected action") {
     forAll(rejected, notifications) { (a, ns) =>
-      val c = a.publish(ns: _*)
+      val c = a.publish(ns.toList: _*)
 
       val ares = a.run.value
       val cres = c.run.value
 
-      assertEquals(cres.decision, ares.decision)
+      assertEquals(cres.result, ares.result)
       assertEquals(cres.notifications, ares.notifications ++ ns)
     }
   }
@@ -132,13 +135,20 @@ class ActionTest extends FunSuite, ScalaCheckSuite {
       val ares = a.run.value
       val bres = b.run.value
 
-      assertEquals(bres.decision, ares.decision) // just for doc
-      assertEquals(bres.notifications, Nil)
+      assertEquals(bres.result, ares.result) // just for doc
+      assertEquals(bres.notifications, Chain.nil)
     }
   }
+
+  checkAll("laws", MonadTests[SUT].monad[Int, Int, String])
+  checkAll("laws", EqTests[SUT[Long]].eqv)
+
+  private given [T: Arbitrary]: Arbitrary[SUT[T]] = Arbitrary(
+    Arbitrary.arbitrary[T].flatMap(t => anyAction.map(_.as(t)))
+  )
 }
 
-object ActionTest {
+object ActionSuite {
   type Rejection = String
   type Event = Int
   type Notification = Long
@@ -147,14 +157,14 @@ object ActionTest {
   type SUT2 = SUT[Long]
 
   private val notification: Gen[Notification] = Arbitrary.arbitrary[Long]
-  private val notifications: Gen[Seq[Notification]] =
-    Gen.containerOf[Seq, Notification](notification)
+  private val notifications: Gen[Chain[Notification]] =
+    Gen.containerOf[Seq, Notification](notification).map(Chain.fromSeq)
 
   private def actionFor(g: Gen[Decision[Rejection, Event, Long]]): Gen[SUT2] =
     for {
       dc <- g
       ns <- notifications
-    } yield Action.liftD(dc).publish(ns: _*)
+    } yield Action.liftD(dc).publish(ns.toList: _*)
 
   val accepted: Gen[SUT2] = actionFor(Generators.accepted)
   val rejected: Gen[SUT2] = actionFor(Generators.rejected)
