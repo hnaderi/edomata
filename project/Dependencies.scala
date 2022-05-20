a1af36eab190e7da292f8b0a4e7a050c1b87119e
Scala Steward <me@scala-steward.org>
2022-05-20 09:32:38 +0200

    Update circe-generic, circe-parser to 0.14.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val CatsEffectMunit = "1.0.7"
     val scalacheckEffectVersion = "1.0.4"
     val doobie = "1.0.0-RC2"
-    val circe = "0.14.1"
+    val circe = "0.14.2"
     val upickle = "2.0.0"
   }
 }
