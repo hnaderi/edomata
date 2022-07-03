0273caa2a8946766efcbf73922c63ee0cd988739
Hossein Naderi <hossein-naderi@hotmail.com>
2022-07-03 14:03:53 +0000

    Update cats-core, cats-laws to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val cats = "2.7.0"
+    val cats = "2.8.0"
     val fs2 = "3.2.9"
     val catsEffect = "3.3.12"
     val skunk = "0.3.1"
