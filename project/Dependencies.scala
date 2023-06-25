b784b6606b57a2b3810aeadb3bd24f6184937c0f
Hossein Naderi <hossein-naderi@hotmail.com>
2023-06-25 01:04:02 +0000

    Update cats-effect, cats-effect-testkit to 3.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val cats = "2.9.0"
-    val catsEffect = "3.5.0"
+    val catsEffect = "3.5.1"
     val fs2 = "3.7.0"
     val skunk = "0.6.0"
     val scalaCheck = "1.15.4"
