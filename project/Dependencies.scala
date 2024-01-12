754cf858acdd2b901ae7009e3139c5bb7e5ba4fc
Hossein Naderi <hossein-naderi@hotmail.com>
2024-01-12 04:22:20 +0330

    Update upickle to 3.1.4 (#296)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val scalacheckEffectVersion = "2.0.0-M2"
     val doobie = "1.0.0-RC5"
     val circe = "0.14.6"
-    val upickle = "3.1.3"
+    val upickle = "3.1.4"
     val jsoniter = "2.27.1"
   }
 }
