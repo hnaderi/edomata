f5427964e20b1eb7ef18331b04da02fefbc0b2a5
Hossein Naderi <hossein-naderi@hotmail.com>
2025-03-17 04:42:55 +0330

    Update circe-generic, circe-parser to 0.14.12 (#456)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val CatsEffectMunit = "2.0.0-M3"
     val scalacheckEffectVersion = "2.0.0-M2"
     val doobie = "1.0.0-RC8"
-    val circe = "0.14.10"
+    val circe = "0.14.12"
     val upickle = "3.2.0"
     val jsoniter = "2.30.1"
   }
